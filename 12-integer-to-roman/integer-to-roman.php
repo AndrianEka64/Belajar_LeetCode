0151f678e1c44d5c118b58fe90d7c3364d48aaae
AndrianEka64 <ekaa18736@gmail.com>
2025-12-03 14:33:26 +0700

    Time: 1 ms (100.00%) | Memory: 20.7 MB (20.51%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.php
@@ -0,0 +1,26 @@
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return String
+     */
+    function intToRoman($num) {
+        $roman = [
+            1000 => "M", 900 => "CM",
+            500 => "D", 400 => "CD",
+            100 => "C", 90 => "XC",
+            50 => "L", 40 => "XL",
+            10 => "X", 9 => "IX",
+            5 => "V", 4 => "IV",
+            1 => "I"
+        ];
+        $result = "";
+        foreach ($roman as $roman => $value) {
+             while ($num >= $roman){
+                $result .= $value;
+                $num -= $roman;
+            }
+        }
+        return $result;
+    }
+}
