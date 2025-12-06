78516c9aee72bb5bafc1c662f6827d2aee1326b9
AndrianEka64 <ekaa18736@gmail.com>
2025-12-06 14:06:59 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.php
@@ -0,0 +1,19 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function climbStairs($n) {
+        if($n == 0){
+            return 0;
+        }
+        if($n == 1){
+            return 1;
+        }
+        if($n == 2){
+            return 2;
+        }
+        return ($n - 1)+( $n - 2);
+    }
+}
