9fbced5248de0e7f6d2f5e4e111693b88599a6c6
AndrianEka64 <ekaa18736@gmail.com>
2025-12-08 11:20:53 +0700

    Time: 27 ms (55%), Space: 48.6 MB (30%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.php
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function minDepth($root) {
+        if($root == null){
+            return 0;
+        }
+        if($root->left == null){
+            return $this->minDepth($root->right) + 1;
+        }
+        if($root->right == null){
+            return $this->minDepth($root->left) + 1;
+        }
+        return min($this->minDepth($root->left),$this->minDepth($root->right))+1;
+    }
+}
